dc4c0d61498d19767f34004e9da3eafd5eb96728
Ferry Hinardi <fhinardi@gmail.com>
2018-07-30 18:09:29 +0700

    update script publish

--- source/www/public/electron.js
+++ source/www/public/electron.js
@@ -39,6 +39,8 @@ function createWindow () {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', () => {
+  console.log('app version...', app.getVersion()); // eslint-disable-line
+
   createWindow();
 });
 
