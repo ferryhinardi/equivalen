e8aa36852b28838655d1b7a535b5890d73769d9f
ferryhinardi <ferry_hinardi@yahoo.co.id>
2018-09-18 10:08:02 +0700

    add dependency spectron and mocha for testing

--- source/www/test/index.js
+++ source/www/test/index.js
@@ -1,34 +1,30 @@
+/* eslint-env node, mocha */
+// Ref: https://www.youtube.com/watch?v=OAsoKBFXeGw
 // A simple test to verify a visible window is opened with a title
 const Application = require('spectron').Application
+const path = require('path');
 const assert = require('assert')
 
-const path = '/Applications/MyApp.app/Contents/MacOS/MyApp';
-const titleApp = 'My App';
+// construct path
+const baseDir = path.join(__dirname, '..');
+const electronBinary = path.join(baseDir, 'node_modules', '.bin', 'electron');
 
-const app = new Application({ path })
+// utility function
+const sleep = time => new Promise(resolve => setTimeout(resolve, time));
 
-app.start()
-  .then(() =>
-    // Check if the window is visible
-    app.browserWindow.isVisible()
-  )
-  .then((isVisible) =>
-    // Verify the window is visible
-    assert.equal(isVisible, true)
-  )
-  .then(() =>
-    // Get the window's title
-    app.client.getTitle()
-  )
-  .then((title) =>
-    // Verify the window's title
-    assert.equal(title, titleApp)
-  )
-  .then(() =>
-    // Stop the application
-    app.stop()
-  )
-  .catch((error) =>
-    // Log any failures
-    console.error('Test failed', error.message)
-  );
+describe('Application Launch', () => {
+  const app = new Application({
+    path: electronBinary,
+    args: [baseDir],
+  });
+
+  before(() => app.start());
+
+  after(() => app.stop());
+
+  it('Shows an initial window', async () => {
+    await app.client.waitUntilWindowLoaded();
+    const count = await app.client.getWindowCount();
+    assert.equal(count, 1);
+  });
+});
