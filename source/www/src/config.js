fd28ea127106588f29c866bae59d573b445e9f7d
Ferry Hinardi <ferry_hinardi@yahoo.co.id>
2018-12-11 14:38:16 +0000

    change config storage gcloud url

--- source/www/src/config.js
+++ source/www/src/config.js
@@ -7,7 +7,7 @@ const dev = {
 const prod = {
   // API_HOST: 'http://localhost:4000',
   API_HOST: 'https://equivalen-backend-staging.herokuapp.com',
-  GOOGLE_CLOUD_STORAGE_API_URI: 'https://storage.googleapis.com/equivalen-cms-production',
+  GOOGLE_CLOUD_STORAGE_API_URI: 'https://storage.googleapis.com/equivalen-cms-staging',
 };
 
 const config = process.env.REACT_APP_STAGE === 'prod'
