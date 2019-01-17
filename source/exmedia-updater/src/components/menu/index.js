c6f225e4695e8549cb6a1a74a3057107613a0b16
ferryhinardi <ferry_hinardi@yahoo.co.id>
2019-01-17 15:23:36 +0700

    remove unused file

--- source/exmedia-updater/src/components/menu/index.js
+++ source/exmedia-updater/src/components/menu/index.js
@@ -1,4 +1,3 @@
 export {default as HamburgerMenu} from './HamburgerMenu';
-export {default as MenuPage} from './MenuPage';
 export {default as MenuPage_} from './MenuPage_';
 export {default as TempMenuPage} from './TempMenuPage';
--- source/exmedia-updater/src/components/modal/index.js
+++ source/exmedia-updater/src/components/modal/index.js
@@ -2,4 +2,3 @@ export {default as withModal} from './withModal';
 export {default as ModalTryout} from './ModalTryout';
 export {default as ModalResult} from './ModalResult';
 export {default as ModalConfirmationFinish} from './ModalConfirmationFinish';
-export {default as ModalEditProfile} from './ModalEditProfile';
