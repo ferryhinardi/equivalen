a1fec878a95d04923a7f4bc318f5972f1210ae92
ferryhinardi <ferry_hinardi@yahoo.co.id>
2019-01-17 15:15:46 +0700

    add stand alone exmedia updater

---
+++ source/exmedia-updater/src/components/menu/index.js
@@ -0,0 +1,4 @@
+export {default as HamburgerMenu} from './HamburgerMenu';
+export {default as MenuPage} from './MenuPage';
+export {default as MenuPage_} from './MenuPage_';
+export {default as TempMenuPage} from './TempMenuPage';
---
+++ source/exmedia-updater/src/config.js
@@ -0,0 +1,39 @@
+const local = {
+  API_HOST: 'http://localhost:4000',
+  GOOGLE_CLOUD_STORAGE_API_URI: 'https://storage.googleapis.com/equivalen-cms-staging',
+  ACCOUNT_KIT: {
+    APPID: '269466223664135',
+    CSRF: 'b4HBW0rzQUqa+bnYNMJEpA==',
+    VERSION: 'v1.1',
+  },
+};
+
+const dev = {
+  API_HOST: 'https://equivalen-backend-staging.herokuapp.com',
+  GOOGLE_CLOUD_STORAGE_API_URI: 'https://storage.googleapis.com/equivalen-cms-staging',
+  ACCOUNT_KIT: {
+    APPID: '2281886948759780',
+    CSRF: 'b4HBW0rzqUQa+BnyNmJepA==',
+    VERSION: 'v1.1',
+  },
+};
+
+const prod = {
+  API_HOST: 'https://equivalen-backend-production.herokuapp.com',
+  GOOGLE_CLOUD_STORAGE_API_URI: 'https://storage.googleapis.com/equivalen-cms-production',
+  ACCOUNT_KIT: {
+    APPID: '2281886948759780',
+    CSRF: 'b4HBW0rzqUQa+BnyNmJepA==',
+    VERSION: 'v1.1',
+  },
+};
+
+const config = process.env.REACT_APP_STAGE === 'prod'
+  ? prod
+  : dev;
+
+export default {
+  // Add common config values here
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config,
+};
